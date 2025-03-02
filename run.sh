f5fc19b795a003400f04dc798f4a220adfc114f9
harshalcreation <harshalborkar501@gmail.com>
2025-03-03 04:32:42 +0530

    Initial commit

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
+#!/bin/bash
 export FLASK_APP=app.py
-export FLASK_ENV=development
-export FLASK_DEBUG=1
-
-# Start Flask server
-flask run
+export FLASK_ENV=production
+gunicorn -w 4 -b 0.0.0.0:5000 app:app
